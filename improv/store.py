e058c46353e57a0b220d0ac026fec471d3a88fe7
paulawucu <paulawu@umich.edu>
2025-04-21 19:49:43 -0400

    Add TTL for redis keys to free memory

--- improv/store.py
+++ improv/store.py
@@ -115,7 +115,7 @@ class RedisStoreInterface(StoreInterface):
             # TODO key twice every time. we still need a better solution for
             # TODO this, but it will work now singlethreaded most of the time.
 
-            self.client.set(object_key, pickle.dumps(object, protocol=5), nx=True)
+            self.client.set(object_key, pickle.dumps(object, protocol=5), nx=True, ex=5)
         except Exception:
             logger.error("Could not store object {}".format(object_key))
             logger.error(traceback.format_exc())
