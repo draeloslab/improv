8f55324933d1815b41b1f90b9fc1a5f3fee69185
Anjali Shankar <anjshnkr@umich.edu>
2025-07-10 12:54:17 -0400

    adding improv_setup script

---
+++ improv_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd $HOME/Desktop/Code/improv/ || exit
+
+source ~/miniforge3/etc/profile.d/conda.sh  
+conda activate improv
+
+export PYTHONPATH=$HOME/Desktop/Code/improv:$HOME/Desktop/Code/:$HOME/Desktop/Code/BayesOpt:$PYTHONPATH
+
