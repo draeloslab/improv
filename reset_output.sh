f47cbce05ad86f199a52810f9bf11fc0d985bd6f
Anjali <anjshnkr@umich.edu>
2025-08-15 15:11:49 -0400

    update relative paths

--- reset_output.sh
+++ reset_output.sh
@@ -3,22 +3,22 @@
 DATE=$(date +%m%d%y)
 
 i=1
-while [ -d "demos/live/output_${DATE}_ds$i" ]; do
+while [ -d "experiments/burgess/output_${DATE}_ds$i" ]; do
     ((i++))
 done
 
-mv demos/live/output "demos/live/output_${DATE}_ds$i"
-mkdir demos/live/output
+mv experiments/burgess/output "experiments/burgess/output_${DATE}_ds$i"
+mkdir experiments/burgess/output
 echo "Renamed output folder to output_${DATE}_ds$i"
 
-mv demos/live/global.log "demos/live/output_${DATE}_ds$i"
+mv experiments/burgess/global.log "experiments/burgess/output_${DATE}_ds$i"
 echo "Moved global.log to output_${DATE}_ds$i"
 
-if [ -f "demos/live/output_${DATE}_ds$i/sample_stream0.h5" ]; then
-    cp "demos/live/output_${DATE}_ds$i/sample_stream0.h5" demos/live/output/initialization.h5
+if [ -f "experiments/burgess/output_${DATE}_ds$i/sample_stream0.h5" ]; then
+    cp "experiments/burgess/output_${DATE}_ds$i/sample_stream0.h5" experiments/burgess/output/initialization.h5
     echo "Copied sample_stream0 as new initialization file"
-elif [ -f "demos/live/output_${DATE}_ds$i/sample_stream_end.h5" ]; then
-    cp "demos/live/output_${DATE}_ds$i/sample_stream_end.h5" demos/live/output/initialization.h5
+elif [ -f "experiments/burgess/output_${DATE}_ds$i/sample_stream_end.h5" ]; then
+    cp "experiments/burgess/output_${DATE}_ds$i/sample_stream_end.h5" experiments/burgess/output/initialization.h5
     echo "Copied sample_stream_end as new initialization file"
 else
     echo "Neither sample_stream0 no sample_stream_end were found in output_${DATE}_ds$i"
