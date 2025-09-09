0508411d98bd0854cb897cf3a4ce02d8d99e5b66
paulawucu <paulawu@umich.edu>
2025-09-09 14:34:07 -0400

    Savier lab run 090525: update path, save contours from caiman

--- reset_output.sh
+++ reset_output.sh
@@ -3,22 +3,22 @@
 DATE=$(date +%m%d%y)
 
 i=1
-while [ -d "demos/live/output_${DATE}_ds$i" ]; do
+while [ -d "experiments/savier/output_${DATE}_ds$i" ]; do
     ((i++))
 done
 
-mv demos/live/output "demos/live/output_${DATE}_ds$i"
-mkdir demos/live/output
+mv experiments/savier/output "experiments/savier/output_${DATE}_ds$i"
+mkdir experiments/savier/output
 echo "Renamed output folder to output_${DATE}_ds$i"
 
-mv demos/live/global.log "demos/live/output_${DATE}_ds$i"
+mv experiments/savier/global.log "experiments/savier/output_${DATE}_ds$i"
 echo "Moved global.log to output_${DATE}_ds$i"
 
-if [ -f "demos/live/output_${DATE}_ds$i/sample_stream0.h5" ]; then
-    cp "demos/live/output_${DATE}_ds$i/sample_stream0.h5" demos/live/output/initialization.h5
+if [ -f "experiments/savier/output_${DATE}_ds$i/sample_stream0.h5" ]; then
+    cp "experiments/savier/output_${DATE}_ds$i/sample_stream0.h5" experiments/savier/output/initialization.h5
     echo "Copied sample_stream0 as new initialization file"
-elif [ -f "demos/live/output_${DATE}_ds$i/sample_stream_end.h5" ]; then
-    cp "demos/live/output_${DATE}_ds$i/sample_stream_end.h5" demos/live/output/initialization.h5
+elif [ -f "experiments/savier/output_${DATE}_ds$i/sample_stream_end.h5" ]; then
+    cp "experiments/savier/output_${DATE}_ds$i/sample_stream_end.h5" experiments/savier/output/initialization.h5
     echo "Copied sample_stream_end as new initialization file"
 else
     echo "Neither sample_stream0 no sample_stream_end were found in output_${DATE}_ds$i"
