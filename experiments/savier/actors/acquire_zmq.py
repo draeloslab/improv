aca4c1788e89b397d123412f63e3414932806c4e
paulawucu <paulawu@umich.edu>
2025-09-09 14:10:58 -0400

    Savier lab run 090525: acquisition freq set to 7.5 Hz

--- experiments/savier/actors/acquire_zmq.py
+++ experiments/savier/actors/acquire_zmq.py
@@ -260,12 +260,12 @@ class ZMQAcquirer(Actor):
         # elif 'frame' in tag: 
         else:
             t0 = time.time()
-            if self.track %2 == 0:
-                self._collect_frame(finalthing)
-                self.frame_num += 1
+            # if self.track %2 == 0:  # not grabbing every other frame, indent next two lines
+            self._collect_frame(finalthing)
+            self.frame_num += 1
             self.total_times_frame.append(time.time() - t0)
             self.timestamp_frame.append([dt.now(), self.frame_num])
-            self.track += 1
+            # self.track += 1
 
         # elif str(tag) in 'tail':
         #     if not self.tailF:
