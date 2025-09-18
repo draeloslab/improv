dc34f690d6996831995579b8821d636501654a12
paulawucu <paulawu@umich.edu>
2025-09-18 10:59:20 -0400

    adding a calibration stimulus actor to run stimulus calibration set

--- experiments/savier/gen_stim_calibrate.py
+++ experiments/savier/gen_stim_calibrate.py
@@ -9,33 +9,39 @@ class StimulusSpace():
         x2 = np.array([0.0, 0.02, 0.04, 0.06, 0.08, 0.10, 0.12]) 
         x3 = np.array([50, 137, 225, 312, 400])
         x4 = np.array([1, 3, 10, 20])
-        x5 = np.array([10, 50, 150])
-        x6 = np.array([1200, 1500, 1750])
+        x5 = np.array([800, 850, 900])
+        x6 = np.array([950, 1000, 1050])
         x7 = np.array([0,1])
 
-        labels = ['angle', 'vel', 'size', 'frequency', 'x_pos', 'y_pos', 'shape']
+        labels = ['angle', 'velocity', 'size', 'frequency', 'center_x', 'center_y', 'shape']
         stim = np.array([x1, x2, x3, x4, x5, x6, x7], dtype=object)
 
         ## 
-        self.initial_stim_count = 8
-        initial_stim = self.initial_stim(stim, initial_type='baseline')
+        
+        # initial_stim = self.initial_stim(stim, initial_type='baseline')
 
         drift_gratings = [[i, 2, 0, 0, 0, 0, 1] for i in range(len(stim[0]))]
-        stationary_spots = [[0,0,0,0,0,0,0], [0,0,0,0,0,2,0], [0,0,0,0,2,2,0], [0,0,0,0,2,0,0], [0,0,0,0,1,1,0]]
+        stationary_spots = [[0,0,0,0,1,1,0], [0,0,0,0,0,0,0], [0,0,0,0,0,2,0], [0,0,0,0,2,2,0], [0,0,0,0,2,0,0]]
         moving_spots = [[0,2,0,1,0,0,0], [0,2,0,1,0,2,0], [0,2,0,1,2,2,0], [0,2,0,1,2,0,0], [0,2,0,1,1,1,0]]
 
-        initial_stim = stationary_spots
+        initial_stim = stationary_spots 
         # initial_stim = drift_gratings + stationary_spots + moving_spots
         # random.shuffle(initial_stim)
 
+        self.initial_stim_count = len(initial_stim)
 
         total_stim_time = 10 # duration of stimuli (in sec)
+        hold_after = 5 # hold after period  (in sec)
+        stationary_t = 0 # stationary time in the beginning (in sec)
+
         # put stimuli, labels, and initial stim in dictionary
         self.stim_space = {
             'stimuli': stim,
             'labels': labels,
             'initial_stim': initial_stim,
-            'total_stim_time': total_stim_time
+            'total_stim_time': total_stim_time,
+            'hold_after': hold_after,
+            'stat_t': stationary_t, 
         }
 
     def initial_stim(self, stim, initial_type):
