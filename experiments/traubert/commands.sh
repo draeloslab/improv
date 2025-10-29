74f0634c22a91adf0976fe9c46f960a6284260fb
Jonathan Gould <jonathan.d.gould@gmail.com>
2025-10-29 17:02:14 -0400

    creates a traubert experiment directory

---
+++ experiments/traubert/commands.sh
@@ -0,0 +1,17 @@
+improv cleanup
+improv run traubert_simulate.yaml
+# coverage run --save-signal=USR1 $(which improv) run traubert_simulate.yaml
+stop
+quit
+pkill -USR1 improv
+improv cleanup
+pkill -9 improv
+
+coverage combine
+coverage html && open htmlcov/index.html 1>/dev/null 2>/dev/null
+
+
+python -m ipdb $(which improv) run traubert_simulate.yaml
+python -m ipdb $(which improv) server -c 0 -o 0 -l 0 -f global.log traubert_simulate.yaml
+
+improv run traubert_simulate.yaml
---
+++ experiments/traubert/convert_owens_data.py
@@ -0,0 +1,12 @@
+import os
+import glob
+import caiman as cm
+
+if __name__ == '__main__':
+    fld = '/mnt/data/datasets/owen_25_09_03'
+    fls = glob.glob(os.path.join(fld,'*.tif'))
+    fls.sort()
+    m = cm.load_movie_chain(fls[:5])
+    split_i = 500
+    m[:split_i].save("./data/a.hdf5")
+    m[split_i:].save("./data/b.hdf5")
