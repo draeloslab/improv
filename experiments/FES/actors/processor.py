cdaaa018cd97d9e06d9aa2b8eff0cb9544345861
jake-joseph <>
2026-01-15 13:31:43 -0500

    1/15 experiments

--- experiments/FES/actors/processor.py
+++ experiments/FES/actors/processor.py
@@ -61,7 +61,7 @@ class Processor(Actor):
             # train_dir = Path("/home/chesteklab/Desktop/dlc-models-pytorch/iteration-2/manipulandum_pytorchMay13-trainset95shuffle1/train")
             # train_dir = Path("/home/chesteklab/Desktop/human-manipulandum-jake-2025-10-29/dlc-models-pytorch/iteration-0/human-manipulandumOct29-trainset95shuffle2/train")
             pytorch_config_path = train_dir / "pytorch_config.yaml"
-            snapshot_path = train_dir / "snapshot-best-010.pt"
+            snapshot_path = train_dir / "snapshot-best-090.pt"
 
             # for top-down models, otherwise None
             detector_snapshot_path = None
@@ -139,11 +139,15 @@ class Processor(Actor):
         """Stop function for saving results and cleaning up."""
         if self.pred_active:
             self.done = True
-            np.save(self.out_folder / "latencies.npy", self.latencies)
-            np.save(self.out_folder / "predictions.npy", self.predictions)
-            np.save(self.out_folder / "startLatencies.npy", self.start_time)
-            np.save(self.out_folder / "dlcLatencies.npy", self.dlc_latencies)
-            np.save(self.out_folder / "latenciesFull.npy", self.latenciesFull)
+            # np.save(self.out_folder / "latencies.npy", self.latencies)
+            np.save(self.out_folder / f"predictions_cam{self.camera_num}.npy", self.predictions)
+            np.save(self.out_folder / f"latencies_cam{self.camera_num}.npy", self.latencies)
+            np.save(self.out_folder / f"startLatencies_cam{self.camera_num}.npy", self.start_time)
+            np.save(self.out_folder / f"dlcLatencies_cam{self.camera_num}.npy", self.dlc_latencies)
+            np.save(self.out_folder / f"latenciesFull_cam{self.camera_num}.npy", self.latenciesFull)
+            # np.save(self.out_folder / "startLatencies.npy", self.start_time)
+            # np.save(self.out_folder / "dlcLatencies.npy", self.dlc_latencies)
+            # np.save(self.out_folder / "latenciesFull.npy", self.latenciesFull)
 
             # np.save(self.out_folder / "grabLatencies.npy", self.grab_latencies)
             # np.save(self.out_folder / "putLatencies.npy", self.put_latencies)
@@ -192,12 +196,12 @@ class Processor(Actor):
                     frame = cv2.resize(frame, (int(frame.shape[1] * self.resize), int(frame.shape[0] * self.resize)))
                     # Convert BGR to RGB for the PyTorch model (trained with RGB images)
                     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                    raw_prediction = self.pose_runner.inference([frame_rgb])  # this needs to be switched back to just frame for camera input
+                    raw_prediction = self.pose_runner.inference([frame])  # this needs to be switched back to just frame for camera input
                     self.dlc_latencies.append(time.perf_counter() - dlc_start)
                     # Extract the bodyparts array from the prediction dictionary
                     # The format is [{'bodyparts': array([[[x, y, likelihood], ...]])}]
                     self.prediction = raw_prediction[0]['bodyparts'][0]  # Get the first (and only) frame's bodyparts
-                    logger.info(f"Shape of prediction: {self.prediction.shape}")
+                    # logger.info(f"Shape of prediction: {self.prediction.shape}")
                     # Select bodyparts based on camera number
                     if self.camera_num == 0:
                         # Use first 4 bodyparts for camera 0
