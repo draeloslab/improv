0986cfdef8012ff40985b1eb9d22d050dfa59582
paulawucu <paulawu@umich.edu>
2026-02-23 23:19:05 -0500

    cleaning reset output bash file; copy bayesopt param yaml file over

--- reset_output.sh
+++ reset_output.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
 DATE=$(date +%m%d%y)
+BASE_DIR="experiments/burgess"
 
 i=1
-while [ -d "experiments/burgess/output_${DATE}_ds$i" ]; do
+while [ -d "${BASE_DIR}/output_${DATE}_ds$i" ]; do
     ((i++))
 done
 
-mv experiments/burgess/output "experiments/burgess/output_${DATE}_ds$i"
-mkdir experiments/burgess/output
+NEW_FOLDER="${BASE_DIR}/output_${DATE}_ds$i"
+
+mv "${BASE_DIR}/output" "$NEW_FOLDER"
+mkdir "${BASE_DIR}/output"
 echo "Renamed output folder to output_${DATE}_ds$i"
 
-mv experiments/burgess/global.log "experiments/burgess/output_${DATE}_ds$i"
+mv "${BASE_DIR}/global.log" "$NEW_FOLDER"
 echo "Moved global.log to output_${DATE}_ds$i"
 
-if [ -f "experiments/burgess/output_${DATE}_ds$i/sample_stream0.h5" ]; then
-    cp "experiments/burgess/output_${DATE}_ds$i/sample_stream0.h5" experiments/burgess/output/initialization.h5
+cp "${BASE_DIR}/bayesopt_parameters.yaml" "$NEW_FOLDER/"
+echo "Archived parameters to $NEW_FOLDER"
+
+if [ -f "$NEW_FOLDER/sample_stream0.h5" ]; then
+    cp "$NEW_FOLDER/sample_stream0.h5" "${BASE_DIR}/output/initialization.h5"
     echo "Copied sample_stream0 as new initialization file"
-elif [ -f "experiments/burgess/output_${DATE}_ds$i/sample_stream_end.h5" ]; then
-    cp "experiments/burgess/output_${DATE}_ds$i/sample_stream_end.h5" experiments/burgess/output/initialization.h5
+elif [ -f "$NEW_FOLDER/sample_stream_end.h5" ]; then
+    cp "$NEW_FOLDER/sample_stream_end.h5" "${BASE_DIR}/output/initialization.h5"
     echo "Copied sample_stream_end as new initialization file"
 else
-    echo "Neither sample_stream0 no sample_stream_end were found in output_${DATE}_ds$i"
+    echo "Neither sample_stream0 nor sample_stream_end were found in $NEW_FOLDER"
 fi
