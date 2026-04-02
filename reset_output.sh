9e6a8ca517a55fd2b1d04472daf92aa3e8d1756a
paulawucu <paulawu@umich.edu>
2026-04-02 15:37:46 -0400

    reset_output copy bayesopt parameters to output folder; savier lab imaging session, (040226)

--- reset_output.sh
+++ reset_output.sh
@@ -21,6 +21,10 @@ echo "Renamed output folder to output_${DATE}_ds$i"
 mv ${path}/global.log "${path}/output_${DATE}_ds$i"
 echo "Moved global.log to output_${DATE}_ds$i"
 
+# TODO: add bayesopt_param yaml to the folder as well
+cp "${path}/bayesopt_parameters.yaml" "${path}/output_${DATE}_ds$i"
+echo "Archived parameters to output_${DATE}_ds$i"
+
 if [ -f "${path}/output_${DATE}_ds$i/sample_stream0.h5" ]; then
     cp "${path}/output_${DATE}_ds$i/sample_stream0.h5" ${path}/output/initialization.h5
     echo "Copied sample_stream0 as new initialization file"
