e3a56c9b9cb078b1cd1aef63805266e9d88c6395
paulawucu <paulawu@umich.edu>
2026-07-02 14:10:17 -0400

    minor bug fixed when we dont have init files. completely moved saving function to vidsaver

--- experiments/common_actors/video_saver.py
+++ experiments/common_actors/video_saver.py
@@ -1,20 +1,10 @@
 import os
 import time
 import h5py
-import numpy as np
-# import zmq
-# import json
-# import pathlib
-# from pathlib import Path
-from improv.actor import Actor, RunManager
-# from ast import literal_eval as make_tuple
-# import matplotlib.pyplot as plt
 import pickle 
-# import re
-# import ast
-# import struct
-# from datetime import datetime as dt
 import cv2
+import numpy as np
+from improv.actor import Actor, RunManager
 from queue import Empty
 import logging; logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -22,20 +12,21 @@ logger.setLevel(logging.INFO)
 
 class VideoSaver(Actor):
 
-    def __init__(self, *args, init_filename=None, **kwargs):
+    def __init__(self, *args, init_frame=None, init_filename=None, **kwargs):
         super().__init__(*args, **kwargs)
         self.frame_number = 0
         self.save_ind = 0
+        self.initial_frame_num = init_frame     # Number of frames for initialization
         self.init_filename = init_filename
+        self.dropped_frames_vid = []
         
     
     def setup(self):
         self.saveArray = []
         if not os.path.exists(self.init_filename):
-
             ## Save initial set of frames to output/initialization.h5
             self.kill_flag = False
-            while self.frame_num < self.initial_frame_num:
+            while self.frame_number < self.initial_frame_num:
                 self.runStep()
 
             self.imgs = np.array(self.saveArray)
@@ -59,23 +50,22 @@ class VideoSaver(Actor):
         if frame is not None:
             try:
                 self.frame = self.client.get(frame[0][str(self.frame_number)])
-                # logger.info(f"frame shape: {self.frame.shape}, frame type: {type(self.frame)}")
                 self.saveArray.append(self.frame)
-                # logger.info(f"getting {self.frame_number} frames, self.saveArray length: {len(self.saveArray)}")
             except ObjectNotFoundError:
                 logger.error("VidSaver: Frame {} unavailable from store, droppping"
                              .format(self.frame_number))
-                # self.dropped_frames.append(self.frame_number)
+                self.dropped_frames_vid.append(self.frame_number)
             except KeyError as e:
                 logger.error("VidSaver: Key error... {0}".format(e))
                 # Proceed at all costs
-                # self.dropped_frames.append(self.frame_number)
+                self.dropped_frames_vid.append(self.frame_number)
             except Exception as e:
                 logger.error("VidSaver error: {}: {} during frame number {}"
                              .format(type(e).__name__, e, self.frame_number))
                 print(traceback.format_exc())
-                # self.dropped_frames.append(self.frame_number)
+                self.droppedropped_frames_vidd_frames.append(self.frame_number)
             self.frame_number += 1
+
         if len(self.saveArray) >= 1000:
             self.imgs = np.array(self.saveArray)
             logger.info(f"saving savearray {self.imgs.shape}")
@@ -90,7 +80,6 @@ class VideoSaver(Actor):
     def _obtain_new_img(self):
         try:
             res = self.q_in.get(timeout=0.0005)
-            # self.frame_number += 1
             return res
         # TODO: add'l error handling
         except Empty:
